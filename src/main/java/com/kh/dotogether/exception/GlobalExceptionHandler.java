f801139a49dbec9303176dde270de443eb6f948f
chlsun <cus5054317@naver.com>
2025-07-20 20:46:50 +0900

    merge

--- src/main/java/com/kh/dotogether/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kh/dotogether/exception/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.kh.dotogether.exception;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.kh.dotogether.exception.exceptions.CustomException;
-import com.kh.dotogether.global.enums.ErrorCode;
 import com.kh.dotogether.util.ResponseError;
 
 @RestControllerAdvice
@@ -22,10 +25,26 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseError);
 	}
 	
+	// 로직실행 중 발생하는 예외 처리
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ResponseError> handleCustomException(CustomException e){
 		
 		return exceptionHandler(e.getCode(), e.getMessage());
+	}
+	
+	// @valid 유효성 검증에 실패했을 경우 발생하는 예외 처리
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handleArgumentNotValid(MethodArgumentNotValidException e) {
+		
+		Map<String, String> errors = new HashMap<>();
+		
+		e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+		
+		ResponseError responseError = ResponseError.builder().code("106")
+															 .messages(errors)
+															 .build();
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseError);
 	}
 	
 }
